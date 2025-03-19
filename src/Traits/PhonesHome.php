7012d0bb2a35df53928864a5c2a19b8722da03fd
Luc Princen <lucprincen@users.noreply.github.com>
2025-03-19 14:25:14 +0100

    fixes an issue for php 7.4 where traits can't have constants for some reason.

--- src/Traits/PhonesHome.php
+++ src/Traits/PhonesHome.php
@@ -2,7 +2,6 @@
 
 namespace ShopMaestro\Conductor\Traits;
 
-use WC_Admin_Notices;
 
 /**
  * Trait PhonesHome
@@ -13,16 +12,17 @@ use WC_Admin_Notices;
 trait PhonesHome {
 
 	/**
-	 * The url constants with which we communicate to the world outside.
+	 * Send a post request
+	 *
+	 * @param string $endpoint
+	 * @param array $params
+	 * @return array|\WP_Error
 	 */
-	protected const API_URL = 'https://wooping.io/wp-json/wooping/v1/';
-
-
 	public function post_request( string $endpoint, array $params = [] ): array|\WP_Error {
 		
 		// Create the request url.
-		$url = apply_filters( 'shop-maestro/conductor/api-url', self::API_URL ) . $endpoint . '/' . $params['plugin'];
-		$request = wp_remote_post( $url, [
+		$url = $this->get_api_url() . $endpoint . '/' . $params['plugin'];
+		$request = \wp_remote_post( $url, [
 			'body'    => json_encode( $params ),
 			'method'  => 'POST',
 			'headers' => [
@@ -31,20 +31,28 @@ trait PhonesHome {
 		]);
 		
 		// Check if we're not encountering an error.
-		if( is_wp_error( $request ) ){
+		if( \is_wp_error( $request ) ){
 			return $request;
 		}
 
 		// Confirm we're dealing with a JSON response
-		$content_type = wp_remote_retrieve_header($request, 'content-type');
+		$content_type = \wp_remote_retrieve_header($request, 'content-type');
 		if (!$content_type || stripos($content_type, 'application/json') === false) {
 			return new \WP_Error( 'invalid_response', 'Invalid response format - expected JSON' );
 		}
 
 		// Return the body
-		$json = wp_remote_retrieve_body( $request );
+		$json = \wp_remote_retrieve_body( $request );
 
 		return json_decode( $json, true );
+	}
+
+
+	/**
+	 * Return our api url
+	 */
+	public function get_api_url(): string {
+		return \apply_filters( 'shop-maestro/conductor/api-url', 'https://wooping.io/wp-json/wooping/v1/' );
 	}
 
 }
