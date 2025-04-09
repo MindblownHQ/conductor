153340db9d8a0ee5fca11cdc2abd33208e28e907
Mark <mark@yourmark.nl>
2025-04-09 09:41:50 +0200

    Install Vite

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+    root: "assets/src",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                "dashboard": path.resolve(__dirname, "/scss/dashboard.scss"),
+				"grid": path.resolve(__dirname, "/js/grid.js"),
+            },
+            output: {
+				entryFileNames: "js/[name].js",
+                assetFileNames: "css/[name].css",
+            },
+        },
+    },
+    css: {
+        postcss: "./config/postcss.config.js"
+    },
+});
