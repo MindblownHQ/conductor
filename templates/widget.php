27eb1212986ba8026df475d5017ed2ef94557d1c
Mark <mark@yourmark.nl>
2025-04-09 09:52:02 +0200

    Add default widget

---
+++ templates/widget.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template for widgets on the Shop Maestro Dashboard.
+ *
+ * @var string $title
+ * @var string $content
+ * @var bool   $draggable
+ * @var bool   $resizable
+ * @var int    $columns
+ * @var int    $rows
+ */
+?>
+
+<div class="widget" draggable="<?php echo $draggable; ?>" data-resizable="<?php echo $resizable; ?>" style="grid-column: span 2">
+	<div class="widget__header">
+		<h2 class="widget__title"><?php echo esc_html( $title ); ?></h2>
+	</div>
+	<div class="widget__content widget__content--with-padding">
+		<?php echo apply_filters( 'the_content', $content ); ?>
+	</div>
+</div>
