10667c871bbc5166eac4cebf6aca75803b6a7428
Marinus Klasen <marinus@mklasen.nl>
2025-04-16 14:25:59 +0200

    do not prefix composer

--- config/php-scoper/dependency-injection.inc.php
+++ config/php-scoper/dependency-injection.inc.php
@@ -6,6 +6,21 @@ use Isolated\Symfony\Component\Finder\Finder;
 
 return [
     'prefix' => 'Shop_Maestro_Vendor', // Change to your desired namespace prefix
+	'exclude-files' => [
+        // Prevent scoping Composer's internal autoloader
+        __DIR__ . '/vendor/composer/autoload_real.php',
+        __DIR__ . '/vendor/composer/autoload_static.php',
+    ],
+
+    'patchers' => [
+        // Prevent scoping internal Composer classes
+        function (string $filePath, string $prefix, string $content): string {
+            if (strpos($filePath, '/vendor/composer/') !== false) {
+                return str_replace("{$prefix}\\Composer\\", 'Composer\\', $content);
+            }
+            return $content;
+        },
+    ],
     'finders' => [
         Finder::create()
             ->files()
